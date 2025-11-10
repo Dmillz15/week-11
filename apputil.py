f769a551544e6bf51fa184c89341c528a8d2b9a2
Dmillz15 <160161559+Dmillz15@users.noreply.github.com>
2025-11-10 18:54:44 +0000

    Move kmeans to apputil and add kmeans_diamonds for exercise 2. import both to the exercises file.

--- apputil.py
+++ apputil.py
@@ -1,1 +1,18 @@
-# your code here
+import seaborn as sns
+import numpy as np
+from sklearn.cluster import KMeans
+
+diamonds = sns.load_dataset("diamonds")
+
+diamonds_numeric = diamonds.select_dtypes(include=[np.number])
+
+def kmeans(X, k):
+    X = np.array(X)
+    model = KMeans(n_clusters=k, random_state=42, n_init=10)
+    model.fit(X)
+    return model.cluster_centers_, model.labels_
+
+def kmeans_diamonds(n, k):
+  
+    X_subset = diamonds_numeric.head(n)
+    return kmeans(X_subset, k)
